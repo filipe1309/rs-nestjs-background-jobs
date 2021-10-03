ea2e48e8dc1a1cc90298d5e354f3cc9ccca7874b
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-10-03 16:37:23 -0300

    feat: send email sync

--- src/create-user/create-user.controller.ts
+++ src/create-user/create-user.controller.ts
@@ -1,11 +1,18 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDTO } from './create-user-dto';
 
 @Controller('create-user')
 export class CreateUserController {
-  
+  constructor(private mailService: MailerService) {}
+
   @Post('/')
-  createUser(@Body() createUserDto: CreateUserDTO) {
-    return createUserDto;
+  async createUser(@Body() createUserDto: CreateUserDTO) {
+    this.mailService.sendMail({
+      to: createUserDto.email,
+      from: 'DOTR <dotr@br.com>',
+      subject: 'Welcome to DOTR',
+      text: `Hello ${createUserDto.name}, your accout has been created. Welcome!`, 
+    });
   }
 }
